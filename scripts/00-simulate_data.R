3b34911c12b385779339a039ab5fb4f0c22934fa
qizhou xie <qizhou.xie@mail.utoronto.ca>
2024-09-25 14:24:22 -0400

    anything

---
+++ scripts/00-simulate_data.R
@@ -0,0 +1,19 @@
+#### Preamble ####
+# Purpose: Simulates... [...UPDATE THIS...]
+# Author: qizhou xie [...UPDATE THIS...]
+# Date: 11 February 2023 [...UPDATE THIS...]
+# Contact: qizhou.xie@mail.utoronto.ca [...UPDATE THIS...]
+# License: MIT
+# Pre-requisites: [...UPDATE THIS...]
+# Any other information needed? [...UPDATE THIS...]
+
+
+#### Workspace setup ####
+library(tidyverse)
+# [...UPDATE THIS...]
+
+#### Simulate data ####
+# [...ADD CODE HERE...]
+
+
+
